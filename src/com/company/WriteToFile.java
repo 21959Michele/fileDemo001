a04d923c9bed7bd6a4675e9c13fb2ded7b85d876
Michele Sousa <21959@student.dorset-college.ie>
2020-11-12 10:53:34 +0000

    first commit

---
+++ src/com/company/WriteToFile.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
+public class WriteToFile {
+
+    private static Formatter output;
+
+    private static String fileName = "superheroes.txt";
+
+    public void CreateTextFile() {
+        OpenFile();
+        AddRecords();
+        CloseFile();
+
+    }
+
+    public void CreateTextFile(String _filename) {
+        fileName = _filename;
+        OpenFile();
+        // AddRecords();
+        AddCapitalCities();
+        CloseFile();
+
+    }
+
+
+
+
+    public static void OpenFile() {
+
+        try {
+            output = new Formatter(fileName);
+        }
+        catch (SecurityException securityException) {
+            System.out.println("Write permission denied");
+            System.exit(1);
+        }
+        catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Error opening file, Terminating");
+            System.exit(1);
+        }
+
+    }
+    public static void AddRecords() {
+
+
+        for(int i = 0; i < 10; i++) {
+            output.format("This is line %d %n",i);
+        }
+
+    }
+
+    public static void AddCapitalCities() {
+
+
+
+        output.format("Ireland %s %n","Dublin");
+        output.format("England %s %n","London");
+        output.format("France %s %n","Paris");
+
+
+    }
+
+
+
+
+
+    public static void CloseFile() {
+
+        if (output != null) {
+            output.close();
+        }
+
+
+    }
+
+
+}
